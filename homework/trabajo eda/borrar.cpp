ea1e37ff63fd2cb730ce988c1263add70059b975
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2023-01-13 16:28:02 -0500

    format some exercises

--- homework/trabajo eda/borrar.cpp
+++ homework/trabajo eda/borrar.cpp
@@ -1,10 +1,9 @@
 #include <dos.h>
-void Modo13h(void)
-{
-        union REGS r;
 
-        r.h.ah=0;
-        r.h.al=0x13;
-        int86(0x10, &r, &r);
+void Modo13h(void) {
+    union REGS r;
+
+    r.h.ah = 0;
+    r.h.al = 0x13;
+    int86(0x10, & r, & r);
 }
-
